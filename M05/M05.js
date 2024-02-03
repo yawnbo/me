79ec2d521f6753da43e4d7a2b6ba6b051bb202b1
Yan Bogdanovskyy <132106589+yawnbo@users.noreply.github.com>
2024-02-02 18:06:43 -0800

    Update M05.js
    oopsie

--- M05/M05.js
+++ M05/M05.js
@@ -3,9 +3,10 @@ var i = 1;                       // Set counter to 1
 var table;                       // Number to multiply by
 
 // Prompt user for type of table.
-while (table > 10 || table < 1) {
+do {
     table = prompt("Enter a number: ");
-}
+} while (table > 10 || table < 1); 
+
 // Conditional to check if user wants to multiply.
 if (operator === 'multiplication') {
   
