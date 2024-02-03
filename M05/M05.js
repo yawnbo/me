25eeabed13798cbd2422d9ca2601682492f8a1fd
Yan Bogdanovskyy <132106589+yawnbo@users.noreply.github.com>
2024-02-02 18:01:17 -0800

    Update M05.js
    update to check for valid input.

--- M05/M05.js
+++ M05/M05.js
@@ -1,20 +1,22 @@
 var operator = 'multiplication'; // Type of calculation
 var i = 1;                       // Set counter to 1
+var table;                       // Number to multiply by
 
 // Prompt user for type of table.
-let table = prompt("Enter a number: ");
-
+while (table > 10 || table < 1) {
+    table = prompt("Enter a number: ");
+}
 // Conditional to check if user wants to multiply.
 if (operator === 'multiplication') {
   
     // Set message to inform of which table is in use.
     var msg = '<h2>Multiplication Table</h2>'
   
-  // Do multiplication and write to msg.
-  while (i < 11) {
-    msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
-    i++;
-  }
+    // Do multiplication and write to msg.
+    while (i < 11) {
+        msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
+        i++;
+    }
 }
 
 // Write msg to page
