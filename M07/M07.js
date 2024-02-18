2801fc6c848d192db0555d39a3c06d013858a010
Yan Bogdanovskyy <132106589+yawnbo@users.noreply.github.com>
2024-02-17 16:45:40 -0800

    M07 init commit

---
+++ M07/M07.js
@@ -0,0 +1,33 @@
+// Create the template for objects that are hotels
+function Hotel(name, rooms, booked) {
+    this.name = name;
+    this.rooms = rooms;
+    this.booked = booked;
+    this.checkAvailability = function() {
+      return this.rooms - this.booked;
+    };
+  }
+  
+  
+  // Create two hotel objects
+  var quayHotel = new Hotel('Quay', 40, 25);
+  var parkHotel = new Hotel('Park', 120, 77);
+  var sunsetHotel = new Hotel('Sunset', 86, 10);
+  
+  // Update the HTML for the page
+  var details1 = quayHotel.name + ' rooms: ';
+      details1 += quayHotel.checkAvailability();
+  var elHotel1 = document.getElementById('hotel1');
+  elHotel1.textContent = details1;
+  
+  var details2 = parkHotel.name + ' rooms: ';
+      details2 += parkHotel.checkAvailability();
+  var elHotel2 = document.getElementById('hotel2');
+  elHotel2.textContent = details2;
+
+  var sunsetDetails = sunsetHotel.name + ' rooms: ';
+      sunsetDetails += sunsetHotel.checkAvailability();
+  var sunsetElement = document.getElementById('hotel3');
+  sunsetElement.textContent = sunsetDetails;
+  
+ 
